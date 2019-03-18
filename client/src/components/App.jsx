a2bf1286b68b4fc7ac9aafbb8bdbd6ec85d7aa1b
cherrih <cherri.hartigan@gmail.com>
2019-03-17 17:55:18 -0700

    Save mouse coordinates to State

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,19 +6,32 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      today: new Date(),      
+      today: new Date(),
+      mouseX: null,
+      mouseY: null   
     };
+    this.mouseMove = this.mouseMove.bind(this);
+  }
+
+  mouseMove(e) {
+    this.setState({
+      mouseX: e.pageX,
+      mouseY: e.pageY
+    })
+  }
+  followMouse() {
+    console.log('I need to move')
   }
 
   render() {
     return (
-      <>
-        <Header/>
+      <div className="body" onMouseMove={this.mouseMove}>
+        <Header mouseX={this.state.mouseX} mouseY={this.state.mouseY}/>
         <Calendar />
         <footer>
           <div className="catch">catch that f*cking idea... </div>
         </footer>
-      </>
+      </div>
     );
   }
 }
