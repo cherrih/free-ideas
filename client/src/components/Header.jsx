6631f266d42cb034ea855664331852a3258ab923
cherrih <cherri.hartigan@gmail.com>
2019-03-17 18:32:18 -0700

    Move divs at top of page with mouseMove

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
 const Header = (props) => {
+  
+  const freeStyle = {
+    left: props.mouseX
+  }
+  const WTFStyle = {
+    left: 100 + props.mouseX
+  }
+  const submitStyle = {
+    left: 200 + props.mouseX
+  }
   return (
     <div className="header-container">
-      <div className="header-bubble free-ideas">
+      <div className="header-bubble free-ideas" style={freeStyle}>
         free
         <br />
         ideas
       </div>
-      <div className="header-bubble WTF">
+      <div className="header-bubble WTF" style={WTFStyle}>
         WTF?!
       </div>
-      <div className="header-bubble submit-an-idea">
+      <div className="header-bubble submit-an-idea" style={submitStyle}>
         submit
         <br />
         an idea
