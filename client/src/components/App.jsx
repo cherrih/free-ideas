e8e9597c15ddfcf1f8118180d9b0a0ad4ef24e95
cherrih <cherri.hartigan@gmail.com>
2019-03-22 18:24:31 -0700

    Explore Google Cal API implementation

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Calendar from './Calendar.jsx';
+import { CALENDAR_ID, API_KEY } from '../../../calendar.config.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +9,8 @@ class App extends React.Component {
     this.state = {
       today: new Date(),
       mouseX: null,
-      mouseY: null   
+      mouseY: null,
+      events: {}
     };
     this.mouseMove = this.mouseMove.bind(this);
   }
@@ -21,6 +23,26 @@ class App extends React.Component {
   }
   followMouse() {
     console.log('I need to move')
+  }
+
+  componentDidMount() {
+    let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(resp => {
+        const events = {}
+        resp.items.map((event) => {
+          const start = event.start.date || event.start.dateTime;
+          events[start] = {
+            end: event.end.date || event.end.dateTime,
+            title: event.summary,
+          }
+        })
+        this.setState({
+          events: events
+        }, () => console.log(this.state.events))
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const express = require('express');
+const request = require('request');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/events');
 
 app.listen(3000, () => {
   console.log('listening on port 3000!');
